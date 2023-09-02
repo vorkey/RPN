7028b52d670c9a5c45f489ef004c3d8f1ad5a1f3
Ambadaya Gaman <livewell@localhost.localdomain>
2023-09-02 19:16:07 +0700

    upload file

---
+++ test.js
@@ -0,0 +1,19 @@
+console.log("Hello world");
+
+// cara membuat variabel
+var a = 10;
+let b = 'Hee';
+const pi = 3.14;
+
+// operator dasar
+let angka1 = 17;
+let angka2 = 56;
+
+console.log(angka1 + angka2, "pertambahan");
+console.log(angka2 - angka1, "pengurangan");
+console.log(angka2 / angka1, "pembagian");
+console.log(angka1 * angka2, "perkalian");
+console.log(angka2 % angka1, "modulus/sisa bagi");
+
+angka1 = angka2 - 17;
+console.log(angka1);
