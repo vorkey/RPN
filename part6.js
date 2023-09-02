c2e616106761b1de7f5d6abd3a5a646df78e860a
Ambadaya Gaman <livewell@localhost.localdomain>
2023-09-02 21:43:26 +0700

    add part6

---
+++ part6.js
@@ -0,0 +1,27 @@
+// Looping
+
+// let i = 0;
+// while (i < 5) {
+//     console.log(i);
+//     i++;
+// }
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+
+// let yay = "peacepeacepeaceyay";
+
+// for (let i = 0; i < yay.length; i++) {
+//     console.log(yay[i])
+// }
+
+// reverse string
+let we = "Waduh";
+let ew = "";
+
+for (let i = we.length - 1; i >= 0; i--) {
+    ew += we[i];
+}
+
+console.log(ew);
