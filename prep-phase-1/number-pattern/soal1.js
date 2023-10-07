cf37b64288763b24c5ab6ad586d5c81cec4e7319
Ambadaya Gaman <ambadayaakun@gmail.com>
2023-10-08 06:20:00 +0700

    number pattern

---
+++ prep-phase-1/number-pattern/soal1.js
@@ -0,0 +1,71 @@
+/*
+================
+MISSING NUMBER
+================
+description: Sebuah fungsi untuk mencari angka yang hilang berdasarkan pola dari board atau
+papan yang tersedia. Fungsi akan mengembalikan nilai sebuah array yang berisi
+angka-angka yang hilang
+
+examples:
+INPUT = 
+[
+  [ 7 ,' ', 5 ],
+  [' ', 8 , 9 ]
+  [ 1 ,' ',' '] 
+]
+
+ASUMSI PADA PAPAN SUDAH TERDAPAT RANGE TERBESAR DAN TERKECIL YAITU 1 DAN 9 SEHINGGA
+OUTPUT:
+[ 2, 3, 4, 6]
+
+PADA MASING-MASING TEST CASE SUDAH TERDAPAT RANGE TERBESAR DAN TERKECIL
+*/
+
+function missingNum(arr) {
+    let tempArr = arr.join().split(",").filter((e) => e != " ");
+    let maxNum = Math.max(...tempArr);
+    let minNum = Math.min(...tempArr);
+    let resArr = [];
+
+    for (let i = minNum; i < maxNum; i++) {
+        if (!tempArr.includes(`${i}`)) {
+            resArr.push(i);
+        }
+    }
+
+    return resArr;
+}
+
+console.log(
+    missingNum([
+        [3, " ", 5],
+        [1, " ", 7],
+        [9, " ", " "],
+    ])
+); // [ 2, 4, 6, 8 ]
+
+console.log(
+    missingNum([
+        [2, " "],
+        [" ", 5],
+    ])
+); // [ 3, 4 ]
+
+console.log(
+    missingNum([
+        [11, " ", 13],
+        [17, " ", 19],
+        [" ", 16, " "],
+    ])
+); // [ 12, 14, 15, 18 ]
+
+console.log(
+    missingNum([
+        [3, " ", 5, 15],
+        [1, " ", 7, 13],
+        [9, " ", " ", 12],
+        [" ", 16, " ", " "],
+    ])
+); // [ 2, 4, 6, 8, 10, 11, 14 ]
+
+console.log(missingNum([])); // []
---
+++ prep-phase-1/number-pattern/soal2.js
@@ -0,0 +1,97 @@
+/**
+  Square Number
+
+  Diberikan sebuah function squareNumber dimana menerima inputan berupa number.
+  Function ini akan mengembalikan array multidimensi yang isi value tersebut secara
+  proses menyerupai `board snakes and ladders` (angka 1 dimulai dari KOLOM ATAS) TAPI
+  alih-alih menuliskan value angka kamu akan menuliskan simbol/karakter dengan syarat sebagai berikut:
+  - jika value merupakan bilangan genap maka diganti menjadi karakter 'o'
+  - jika value merupakan bilangan ganjil maka diganti menjadi karakter 'x'
+  - jika value merupakan kelipatan 4 maka diganti menjadi simbol '#'
+
+  Contoh 1:
+  ==========
+  input: 3
+  proses:
+          [
+            [ 1, 2, 3 ],
+            [ 6, 5, 4 ],
+            [ 7, 8, 9 ]
+          ]
+  output:
+          [
+            [x, o, x],
+            [o, x, #],
+            [x, #, x]
+          ]
+
+  Contoh 2:
+  ==========
+  input: 4
+  proses:
+        [
+          [ 1, 2, 3, 4 ],
+          [ 8, 7, 6, 5 ],
+          [ 9, 10, 11, 12 ],
+          [ 16, 15, 14, 13 ]
+        ]
+  output:
+        [
+          [ x, o, x, # ],
+          [ #, x, o, x ],
+          [ x, o, x, # ],
+          [ #, x, o, x ]
+        ]
+NOTE:
+ - INPUT PARAMETER MINIMAL 3, JIKA KURANG DARI 3 MAKA RETURN 'Minimal input adalah 3'
+**/
+
+function squareNumber(num) {
+    if (num < 3) return "Minimal Input Adalah 3";
+
+    let resArr = [];
+    for (let i = 0, currentNumber = 1; i < num; i++) {
+        resArr.push([]);
+        for (let j = 0; j < num; j++) {
+            resArr[i].push(currentNumber);
+            currentNumber++;
+        }
+    }
+    for (let i = 0; i < resArr.length; i++) {
+        if (i % 2 != 0) {
+            resArr[i].sort((a, b) => b - a);
+        }
+    }
+    resArr.forEach((e, a) => {
+        e.forEach((i, b) => {
+            if (i % 4 == 0) {
+                resArr[a][b] = "#";
+            } else if (i % 2 == 0) {
+                resArr[a][b] = "o";
+            } else {
+                resArr[a][b] = "x";
+            }
+            b++;
+        });
+        a++;
+    });
+    return resArr;
+}
+
+console.log(squareNumber(3));
+// [ [x, o, x],  [o, x, #], [x, #, x] ]
+
+console.log(squareNumber(4));
+// [ [ x, o, x, # ],
+//   [ #, x, o, x ],
+//   [ x, o, x, # ],
+//   [ #, x, o, x ] ]
+
+console.log(squareNumber(5));
+// [ [ x, o, x, #, x ],
+//   [ o, x, #, x, o ],
+//   [ x, #, x, o, x ],
+//   [ #, x, o, x, # ],
+//   [ x, o, x, #, o ] ]
+
+console.log(squareNumber(2)); // Minimal input adalah 3
